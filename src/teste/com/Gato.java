e43056dbbf799c03f7eb4c9fc33606618d62e662
skypillar <ph.panta@gmail.com>
2022-05-27 22:59:17 -0300

    first commit

---
+++ src/teste/com/Gato.java
@@ -0,0 +1,44 @@
+package teste.com;
+
+public class Gato {
+	private String nome;
+	private String cor;
+	private String idade;
+	
+	public Gato() {}
+	
+	public Gato(String nome, String cor, String idade) {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getIdade() {
+		return idade;
+	}
+
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public String toString() {
+		return "Gato [nome=" + nome + ", cor=" + cor + ", idade=" + idade + "]";
+	}
+}
---
+++ src/teste/vambora.java
@@ -0,0 +1,24 @@
+package teste;
+
+import teste.com.Gato;
+
+public class vambora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		System.out.println("Hello world acb");
+	}
+
+}
+
+class Livro {
+	private String nome;
+	private String npgs;
+}
